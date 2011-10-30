4e6ff9281cf2eb8a11444d181c8108c6fed2f16e
Ariel Dela Fuente <ariel@toomuchtech.com>
2011-10-29 22:08:30 -0400

    Problem 6 complete.

--- projecteuler/src/com/arieldelafuente/problem6.scala
+++ projecteuler/src/com/arieldelafuente/problem6.scala
@@ -11,7 +11,7 @@ package com.arieldelafuente
  * 
  * Hence the difference between the sum of the squares of the 
  * first ten natural numbers and the square of the 
- * sum is 3025  385 = 2640.§
+ * sum is 3025 - 385 = 2640
  * 
  * Find the difference between the sum of the squares of the 
  * first one hundred natural numbers and the square of the sum.
@@ -19,6 +19,21 @@ package com.arieldelafuente
 
 object problem6 {
 
-  def main(args: Array[String]): Unit = {}
+  def main(args: Array[String]): Unit = {
+    val prob = 100
+
+    println(sqofsum(prob,0)-sumofsq(prob,0))
+  }
+  
+  def sumofsq(m: Int, n: Long): BigInt = {
+    if (m == 0) return n
+    else sumofsq(m - 1, n + (m*m))
+  }
+  
+  def sqofsum(m: Int, n: Long): BigInt = {
+    if (m == 0) n*n
+    else sqofsum(m - 1, n + m)
+  }
 
 }
+
